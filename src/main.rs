a02ab129cd91eee60f5acf2ae35d0318f12b89bf
steve <steven.pack@gmail.com>
2017-02-21 09:50:13 -0800

    loading ui.glad as resource

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod launcher;
 mod castnow;
 mod ui;
 
+
 fn main() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,8 @@
 extern crate gtk;
 
 use gtk::prelude::*;
-use gtk::{Button, Window, WindowType, Box};
+use gtk::{Button, Box};
+use gtk::Builder;
 use std::rc::Rc;
 use std::cell::RefCell;
 use castnow::{CastNow, KeyCommand};
@@ -9,22 +10,35 @@ use castnow::{CastNow, KeyCommand};
 type SharedCastNow = Rc<RefCell<CastNow>>;
 
 pub fn build() {
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("castnow desktop-rs");
-    window.set_default_size(350, 70);
-    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
 
+    // Load glade file  
+    let glade_str = include_str!("ui.glade");
+    let builder = Builder::new_from_string(glade_str);
+    // Create Window   
+    let window : gtk::Window = builder.get_object("applicationwindow1").unwrap();  
     window.connect_delete_event(|_, _| {
         gtk::main_quit();
         Inhibit(false)
     });
+
+    // Set window size  
+    //window.set_default_size(400,400);  
+
+    // Load components  
+    // let button1 : gtk::Button = builder.get_object("button1").unwrap();  
+    // let label1 : gtk::Label = builder.get_object("label1").unwrap();     
+
+    //let window = Window::new(WindowType::Toplevel);
+    window.set_title("castnow desktop-rs");
+    //window.set_default_size(350, 70);
+    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
 
     let castnow = CastNow::new();
     let shared: SharedCastNow =  Rc::new(RefCell::new(castnow));
 
     let load_button = Button::new_with_label("Load");
     let shared_castnow = shared.clone();
-     load_button.connect_clicked(move |_| {
+    load_button.connect_clicked(move |_| {
         println!("{} clicked!", "Load");
         shared_castnow.borrow_mut().load("/home/steve/Downloads/SampleVideo_1280x720_5mb.mp".to_string());
     });
@@ -40,7 +54,7 @@ pub fn build() {
 
 fn add_button(label: &'static str, shared_castnow: SharedCastNow, cmd: KeyCommand, gtk_box: &Box) {
     let button = Button::new_with_label(label);
-     button.connect_clicked(move |_| {
+    button.connect_clicked(move |_| {
         println!("{} clicked!", label);
         shared_castnow.borrow_mut().execute(&cmd);
     });

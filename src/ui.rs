57b61184da6ad7d9e8a60e8346892ab96306d64f
steve <steven.pack@gmail.com>
2017-02-23 00:48:23 -0800

    Dialog returning and buttons hooked up.
    Lifetime issue with tx

--- src/ui.rs
+++ src/ui.rs
@@ -2,6 +2,7 @@ extern crate gtk;
 
 use gtk::prelude::*;
 use gtk::Builder;
+use gtk::{Window, WindowType};
 use gtk::{Button, FileChooserDialog, Entry};
 use std::sync::mpsc::Sender;
 use castnow::{KeyCommand, Command};
@@ -30,24 +31,45 @@ impl UiBuilder {
         });
 
         let file_chooser_button: Button = Self::get_obj(&builder, "popupFileChooserButton");
-        let file_chooser_dlg: FileChooserDialog = Self::get_obj(&builder, "filechooserdialog1");
+        let file_path_entry: Entry = Self::get_obj(&builder, "filePathEntry1");
+        let load_button: Button = Self::get_obj(&builder, "playButton");
+        let mute_button: Button = Self::get_obj(&builder, "muteButton");
+        let stop_button: Button = Self::get_obj(&builder, "stopButton");
+
+        let file_path_entry1 = file_path_entry.clone();
         file_chooser_button.connect_clicked(move |_| {
             println!("{} clicked!", "Popup file dialog");
-            file_chooser_dlg.show();    
+            
+            let dialog = FileChooserDialog::new (
+                        Some("Choose DireFilectory"),
+                        Some(&Window::new(WindowType::Popup)),
+                        gtk::FileChooserAction::Open,
+            );
+            dialog.add_button("Cancel", gtk::ResponseType::Cancel.into());
+            dialog.add_button("Select", gtk::ResponseType::Ok.into());
+
+            if dialog.run() == gtk::ResponseType::Ok.into() {
+                dialog.get_filename().map(|path| path.to_str().map(|text| file_path_entry1.set_text(text)));
+            }
+            dialog.destroy();   
         });
 
-        let file_path_entry: Entry = Self::get_obj(&builder, "filePathEntry1");
-
-        let load_button: Button = Self::get_obj(&builder, "playButton");
+        let a = tx.clone();
+        let b = tx.clone();
+        let c = tx.clone();
         load_button.connect_clicked(move |_| {
-            let path = file_path_entry.get_text().unwrap_or_default();
-            Self::send(&tx, KeyCommand::Load, path)});
+            let path = file_path_entry.get_text();
+            Self::send(&a, KeyCommand::Load, path)
+        });
+
+        mute_button.connect_clicked(move |_| Self::send(&b, KeyCommand::Mute, None));
+        stop_button.connect_clicked(move |_| Self::send(&c, KeyCommand::Stop, None));
         
         window.show_all();    
     }
 
-    fn send(tx: &Sender<Command>, key: KeyCommand, state: String) {
-        tx.send(Command::new_with_state(key, state)).map_err(|e| println!("Failed to send {:?}", e)).ok();
+    fn send(tx: &Sender<Command>, key: KeyCommand, state: Option<String>) {
+        tx.send(Command::new_with_state(key, state.unwrap_or_default())).map_err(|e| println!("Failed to send {:?}", e)).ok();
     }
 
     fn get_obj<T: gtk::IsA<gtk::Object>>(builder: &Builder, name: &'static str) -> T {
